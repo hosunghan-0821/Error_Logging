69fdfeae1aa772ece84dfc9f251abea6d3fc9229
mury33@naver.com <mury33@naver.com>
2022-07-13 21:09:54 +0900

    프로젝트 완성

--- read.py
+++ read.py
@@ -1,6 +1,15 @@
 from flask import Blueprint, render_template, request, jsonify
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://logging:12345@cluster0.wfh6y.mongodb.net/Cluster0?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.dbsparta
+
 
 read_page = Blueprint('read_page', __name__, template_folder='templates')
 @read_page.route('/log/read')
 def read():
     return render_template('read.html')
+
